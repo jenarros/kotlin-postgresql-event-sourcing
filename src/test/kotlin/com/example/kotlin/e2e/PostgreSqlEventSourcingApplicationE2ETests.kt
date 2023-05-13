01f7fb963f672fac3c46ab0cfe3993f41fa68d81
Eduardo Narros <jenarros@gmail.com>
2023-05-13 21:28:44 +0100

    Migrated e2e test to kotlin

---
+++ src/test/kotlin/com/example/kotlin/e2e/PostgreSqlEventSourcingApplicationE2ETests.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin.e2e
+
+import com.example.eventsourcing.functional.OrderTestScript
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.client.RestTemplateBuilder
+
+class PostgreSqlEventSourcingApplicationE2ETests {
+    @Test
+    fun orderTestScript() {
+        val restTemplate = RestTemplateBuilder().rootUri(ROOT_URI)
+        OrderTestScript(TestRestTemplate(restTemplate), KAFKA_BROKERS).execute()
+    }
+
+    companion object {
+        private const val ROOT_URI = "http://localhost:8080"
+        private const val KAFKA_BROKERS = "localhost:9092"
+    }
+}
