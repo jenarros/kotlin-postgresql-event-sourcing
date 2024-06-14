5f449b9887e964181f781efdc31fdbf5119f0647
Eduardo Narros <jenarros@gmail.com>
2024-06-14 16:58:22 +0100

    Upload test reports to codacy

--- src/main/kotlin/com/example/eventsourcing/config/Json.kt
+++ src/main/kotlin/com/example/eventsourcing/config/Json.kt
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.http4k.format.Jackson
 
 object Json {
